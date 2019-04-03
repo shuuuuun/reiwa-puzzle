0d982fed3de46f097da39f0c45007cfe7b5c720d
motoki-shun <motoki-shun@kayac.com>
2019-04-03 23:43:19 +0900

    gengo-puyo init

---
+++ gengo-puyoTests/gengo_puyoTests.swift
@@ -0,0 +1,34 @@
+//
+//  gengo_puyoTests.swift
+//  gengo-puyoTests
+//
+//  Created by motoki-shun on 2019/04/03.
+//  Copyright © 2019 motoki-shun. All rights reserved.
+//
+
+import XCTest
+@testable import gengo_puyo
+
+class gengo_puyoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
