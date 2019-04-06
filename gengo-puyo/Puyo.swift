6d7b3dd49f937b06f13646273110457411c9853c
motoki-shun <motoki-shun@kayac.com>
2019-04-06 13:48:04 +0900

    ボードっぽいものを表示してみる

---
+++ gengo-puyo/Puyo.swift
@@ -0,0 +1,12 @@
+//
+//  Puyo.swift
+//  gengo-puyo
+//
+//  Created by motoki-shun on 2019/04/04.
+//  Copyright © 2019 motoki-shun. All rights reserved.
+//
+
+import Foundation
+
+class Puyo {
+}
