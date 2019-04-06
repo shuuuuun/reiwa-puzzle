136c3ee64884d30e8d8d68b19459d1a175ac24d0
motoki-shun <motoki-shun@kayac.com>
2019-04-06 15:43:50 +0900

    ざっと移植してboard描画するところまで

--- gengo-puyo/GameScene.swift
+++ gengo-puyo/GameScene.swift
@@ -10,70 +10,30 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    // private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        // self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        // if let label = self.label {
-        //     label.alpha = 0.0
-        //     label.run(SKAction.fadeIn(withDuration: 2.0))
-        // }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            // spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            // spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-            //                                   SKAction.fadeOut(withDuration: 0.5),
-            //                                   SKAction.removeFromParent()]))
-            spinnyNode.strokeColor = SKColor.green
-            self.addChild(spinnyNode)
 
-            let board: [[Int]] = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-            // for row in board {
-            //     for val in row {
-            for (y, row) in board.enumerated() {
-                for (x, _) in row.enumerated() {
-                    if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-                        n.position = CGPoint(x: w * CGFloat(x), y: w * CGFloat(y))
-                        self.addChild(n)
-                    }
-                }
-            }
+    let game = Puyo()
+    let gameUpdateInterval = 1.0
+    var lastUpdateTime: TimeInterval = 0.0
+
+    private var baseStone: SKShapeNode?
+
+    override func didMove(to view: SKView) {
+        let w = (self.size.width + self.size.height) * 0.05
+        self.baseStone = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+                if let stone = self.baseStone {
+            stone.lineWidth = 2.5
+            stone.strokeColor = SKColor.green
         }
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -95,10 +55,36 @@ class GameScene: SKScene {
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         // for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
-    
-    
+
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
-        print(currentTime)
+        // print(currentTime)
+
+        if lastUpdateTime + gameUpdateInterval <= currentTime {
+            self.game.update()
+            // print(self.game.board)
+            lastUpdateTime = currentTime
+        }
+        draw()
+    }
+
+    func draw() {
+        drawBoard()
+        drawCurrentBlock()
+    }
+
+    func drawBoard() {
+        let w = (self.size.width + self.size.height) * 0.05
+        for (y, row) in self.game.board.enumerated() {
+            for (x, _) in row.enumerated() {
+                if let n = self.baseStone?.copy() as! SKShapeNode? {
+                    n.position = CGPoint(x: w * CGFloat(x) - self.size.width/2 + w, y: w * CGFloat(y) - self.size.height/2 + w)
+                    self.addChild(n)
+                }
+            }
+        }
+    }
+
+    func drawCurrentBlock() {
     }
 }
--- gengo-puyo/Puyo.swift
+++ gengo-puyo/Puyo.swift
@@ -8,5 +8,223 @@
 
 import Foundation
 
+struct Stone {
+    var color: String
+
+    init(color: String) {
+        self.color = color
+    }
+}
+
+struct Block {
+    var stones: [Stone]
+    var x: Int = 0
+    var y: Int = 0
+
+    init(stones: [Stone], x: Int, y: Int) {
+        self.stones = stones
+        self.x = x
+        self.y = y
+    }
+
+    mutating func moveLeft() {
+        self.x -= 1
+    }
+
+    mutating func moveRight() {
+        self.x += 1
+    }
+
+    mutating func moveDown() {
+        self.y += 1
+    }
+
+    mutating func rotate() {
+        // TODO: block rotate
+    }
+}
+
 class Puyo {
+    let cols = 6
+    let rows = 12
+
+    let number_of_stone = 2
+
+    let hidden_rows: Int
+    let logical_rows: Int
+
+    // let start_x = Math.floor((coLs - nuMber_of_stone) / 2)
+    // let start_y = 0
+
+    let stone_list: [Stone] = Array(repeating: Stone(color: "green"), count: 10)
+
+    var board: [[Int]]
+    var currentBlock: Block
+    var nextBlock: Block?
+    var isPlayng: Bool = false
+
+    init() {
+        self.hidden_rows = self.number_of_stone
+        self.logical_rows = self.rows + self.hidden_rows
+
+        self.board = Array(repeating: Array(repeating: 0, count: self.cols), count: self.rows)
+
+        self.nextBlock = Block(stones: [stone_list.randomElement()!, stone_list.randomElement()!], x: 0, y: 0)
+        self.currentBlock = Block(stones: [stone_list.randomElement()!, stone_list.randomElement()!], x: 0, y: 0)
+    }
+
+    func newGame() {
+        self.isPlayng = true
+        self.createCurrentBlock()
+    }
+
+    func quitGame() {
+        self.isPlayng = false
+    }
+
+    func update() {
+        if self.moveBlockDown() {
+            return
+        }
+        self.freeze()
+        self.clearLines()
+        if self.checkGameOver() {
+            self.quitGame()
+            return
+        }
+        self.createCurrentBlock()
+        self.createNextBlock()
+    }
+
+    func createCurrentBlock() {
+        if self.nextBlock == nil {
+            self.createNextBlock()
+        }
+        self.currentBlock = self.nextBlock!
+    }
+
+    func createNextBlock() {
+        self.nextBlock = Block(stones: [stone_list.randomElement()!, stone_list.randomElement()!], x: 0, y: 0)
+    }
+
+    func freeze() {
+        // for y := 0; y < number_of_block; y++ {
+        //   for x := 0; x < number_of_block; x++ {
+        //     boardX := x + self.currentBlock.x
+        //     boardY := y + self.currentBlock.y
+        //     if self.currentBlock.shape[y][x] == 0 || boardY < 0 {
+        //       continue
+        //     }
+        //     if self.currentBlock.shape[y][x] != 0 {
+        //       self.board[boardY][boardX] = self.currentBlock.blockId + 1
+        //     } else {
+        //       self.board[boardY][boardX] = 0
+        //     }
+        //   }
+        // }
+    }
+
+    func clearLines() {
+        // var filledRowList []int
+        // for y := logical_rows - 1; y >= 0; y-- {
+        //   isRowFilled := !contains(self.board[y], 0)
+        //   if !isRowFilled {
+        //     continue
+        //   }
+        //   filledRowList = append(filledRowList, y)
+        // }
+        // if len(filledRowList) > 0 {
+        //   var newBoard [][]int
+        //   for range filledRowList {
+        //     blankRow := make([]int, cols)
+        //     newBoard = append(newBoard, blankRow)
+        //   }
+        //   for i, row := range self.board {
+        //     isRowFilled := contains(filledRowList, i)
+        //     if isRowFilled {
+        //       continue
+        //     }
+        //     newBoard = append(newBoard, row)
+        //   }
+        //   self.board = newBoard
+        // }
+    }
+
+    func moveBlockLeft() -> Bool {
+        // isValid := self.validate(-1, 0, self.currentBlock)
+        // if isValid {
+        //   self.currentBlock.moveLeft()
+        // }
+        // return isValid
+        return true
+    }
+
+    func moveBlockRight() -> Bool {
+        // isValid := self.validate(1, 0, self.currentBlock)
+        // if isValid {
+        //   self.currentBlock.moveRight()
+        // }
+        // return isValid
+        return true
+    }
+
+    func moveBlockDown() -> Bool {
+        // // isValid := self.validate(0, 1)
+        // isValid := self.validate(0, 1, self.currentBlock)
+        // if isValid {
+        //   self.currentBlock.moveDown()
+        // }
+        // return isValid
+        return true
+    }
+
+    func rotateBlock() -> Bool {
+        // rotatedBlock := self.currentBlock // copy
+        // rotatedBlock.rotate()
+        // isValid := self.validate(0, 0, rotatedBlock)
+        // if isValid {
+        //   self.currentBlock = rotatedBlock
+        // }
+        // return isValid
+        return true
+    }
+
+    func validate(offsetX: Int, offsetY: Int, block: Block) -> Bool {
+        // // block = block || self.currentBlock
+        // nextX := block.x + offsetX
+        // nextY := block.y + offsetY
+        // if block.shape == nil {
+        //   return false
+        // }
+        // for y := 0; y < number_of_block; y++ {
+        //   for x := 0; x < number_of_block; x++ {
+        //     if block.shape[y][x] == 0 {
+        //       continue
+        //     }
+        //     boardX := x + nextX
+        //     boardY := y + nextY
+        //     isOutsideLeftWall := boardX < 0
+        //     isOutsideRightWall := boardX >= cols
+        //     isUnderBottom := boardY >= logical_rows
+        //     isOutsideBoard := boardY >= len(self.board) || boardX >= len(self.board[boardY])
+        //     if isOutsideLeftWall || isOutsideRightWall || isUnderBottom || isOutsideBoard {
+        //       return false
+        //     }
+        //     if self.board[boardY][boardX] != 0 { // isExistsBlock
+        //       return false
+        //     }
+        //   }
+        // }
+        return true
+    }
+
+    func checkGameOver() -> Bool {
+        // isGameOver := true
+        // boardY := self.currentBlock.y + (number_of_block - 1)
+        // if boardY >= hidden_rows {
+        //   isGameOver = false
+        // }
+        // return isGameOver
+        return false
+    }
 }
