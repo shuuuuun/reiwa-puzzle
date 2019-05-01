a9d949196547d207a2995cd44e1afef441388c81
motoki-shun <motoki-shun@kayac.com>
2019-05-01 10:37:36 +0900

    show more debug info

--- gengo-puyo/GameViewController.swift
+++ gengo-puyo/GameViewController.swift
@@ -31,6 +31,10 @@ class GameViewController: UIViewController {
             
             view.showsFPS = true
             view.showsNodeCount = true
+            view.showsDrawCount = true
+            view.showsQuadCount = true
+            view.showsPhysics = true
+            view.showsFields = true
         }
     }
 
